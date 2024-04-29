40e4e7fb8cde7030de66f63265a569f1848c4213
Fernando-Popolili <fpopolili@gamil.com>
2024-04-29 18:56:13 -0300

    Começando o Formulario para adiconar tarefas na lista

--- Lista-de-Tarefas/src/App.jsx
+++ Lista-de-Tarefas/src/App.jsx
@@ -1,13 +1,42 @@
 import './App.css'
+import { useState } from "react";
+import Todo from './components/Todo';
+import TodoForm from './components/TodoForm';
 
 function App() {
-  
+  // SETTODOS = INSERIR OS DADOS
+    const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text:"Criar funcionalidade X no Sistema",
+      categoria: "Trabalho",
+      completo: false,
+    },
+    {
+      id: 2,
+      text:"Ir para a academia",
+      categoria: "Pessoal",
+      completo: false,
+    },
+
+  ])
 
   return (
-    <>
-      <p>Teste</p>
-    </>
-  )
-}
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {/* Percorrer o ARRAY USANDO O MAP*/}
+        {todos.map((todo)=>(
+          // PUXANDO O COMPONENT TODO.JSX
+          <Todo todo={todo} />
+          
+         
+        ))}
+      </div>
+
+      <TodoForm/>
+    </div>
+  );
+};
 
 export default App
